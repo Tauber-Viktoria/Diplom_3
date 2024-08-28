ca5b17500e9c7e5468755f75e533330fc25d3fc4
Tauber-Viktoria <viktoriia.tauber@gmail.com>
2024-08-28 11:49:35 +0300

    fix allure

--- pages/reset_password_page.py
+++ pages/reset_password_page.py
@@ -9,10 +9,10 @@ class ResetPasswordPage(BasePage):
     def toggle_password_visibility(self):
         self.click_on_element(ResetPasswordLocators.TOGGLE_PASSWORD_BUTTON)
 
-    @allure.step("клик на кнопку 'Показать/Скрыть пароль'")
-    def is_password_field_active(self):
-        return self.is_element_present(ResetPasswordLocators.PASSWORD_INPUT_AFTER)
-
     @allure.step("дождаться поля ввода пароля")
     def wait_for_password_input(self):
         return self.is_element_present(ResetPasswordLocators.PASSWORD_INPUT_BEFORE)
+
+    @allure.step('проверка активации кнопки "показать/скрыть пароль"')
+    def is_password_field_active(self):
+        return self.is_element_present(ResetPasswordLocators.PASSWORD_INPUT_AFTER)
