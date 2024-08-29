59115d34f2fe43d608738d609b6de79518074a1b
Tauber-Viktoria <viktoriia.tauber@gmail.com>
2024-08-29 23:11:15 +0300

    combine two tests into one with parameterization

--- tests/test_feed_page.py
+++ tests/test_feed_page.py
@@ -1,4 +1,5 @@
 import allure
+import pytest
 
 
 class TestFeedPage:
@@ -18,28 +19,36 @@ class TestFeedPage:
         assert feed_page.get_order_id_from_list_ready() == f'0{order_id}', \
             f"Ожидался ID заказа '0{order_id}', но получен {feed_page.get_order_id_from_list_ready()}"
 
+    @pytest.mark.parametrize(
+        "counter_name, get_initial_count, get_new_count, story, title",
+        [
+            (
+                    "за все время",
+                    lambda feed_page: feed_page.get_number_orders_all_time(),
+                    lambda feed_page: feed_page.get_number_orders_all_time(),
+                    "При создании нового заказа счётчик Выполнено за всё время увеличивается",
+                    'Тест на увеличение счётчика заказов "за все время" после добавления заказа',
+            ),
+            (
+                    "за сегодня",
+                    lambda feed_page: feed_page.get_number_orders_today(),
+                    lambda feed_page: feed_page.get_number_orders_today(),
+                    "При создании нового заказа счётчик Выполнено за сегодня увеличивается",
+                    'Тест на увеличение счётчика заказов "за сегодня" после добавления заказа',
+            ),
+        ],
+    )
     @allure.feature('Раздел «Лента заказов»')
-    @allure.story('При создании нового заказа счётчик Выполнено за всё время увеличивается')
-    @allure.title('Тест на увеличение счётчика заказов "за все время" после добавления заказа')
-    def test_order_counter_all_time_increases(self, main_page, feed_page, login_in):
-        main_page.click_order_feed_button() # Переход в ленту заказов
-        initial_count = feed_page.get_number_orders_all_time() # Зафиксировать текущее значение счётчика заказов
-        main_page.click_constructor_button() # Переход в конструктор
-        main_page.create_order() # Создать заказ
-        main_page.click_order_feed_button() # Переход в ленту заказов
-        new_count = feed_page.get_number_orders_all_time() # Зафиксировать новое значение счётчика
-        assert new_count == initial_count + 1, \
-            f"Ожидалось, что счётчик увеличится на 1, но он изменился с {initial_count} на {new_count}."
+    def test_order_counter_increases(self, main_page, feed_page, login_in,
+                                     counter_name, get_initial_count, get_new_count, story, title):
+        allure.dynamic.story(story)
+        allure.dynamic.title(title)
 
-    @allure.feature('Раздел «Лента заказов»')
-    @allure.story('При создании нового заказа счётчик Выполнено за сегодня увеличивается')
-    @allure.title('Тест на увеличение счётчика заказов "за сегодня" после добавления заказа')
-    def test_order_counter_all_time_increases(self, main_page, feed_page, login_in):
         main_page.click_order_feed_button() # Переход в ленту заказов
-        initial_count = feed_page.get_number_orders_today() # Зафиксировать текущее значение счётчика заказов
+        initial_count = get_initial_count(feed_page) # Зафиксировать текущее значение счётчика заказов
         main_page.click_constructor_button() # Переход в конструктор
         main_page.create_order() # Создать заказ
         main_page.click_order_feed_button() # Переход в ленту заказов
-        new_count = feed_page.get_number_orders_today() # Зафиксировать новое значение счётчика
+        new_count = get_new_count(feed_page) # Зафиксировать новое значение счётчика
         assert new_count == initial_count + 1, \
             f"Ожидалось, что счётчик увеличится на 1, но он изменился с {initial_count} на {new_count}."
